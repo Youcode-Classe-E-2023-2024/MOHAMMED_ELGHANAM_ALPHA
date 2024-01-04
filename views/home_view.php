f19d0f8a40e6448dfed91b50c300c87b097fcf0c
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-01-04 15:13:27 +0100

    add page blog

--- views/home_view.php
+++ views/home_view.php
@@ -24,7 +24,7 @@
                         <a href="index.php?page=dashboard" class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
                             Go to dashboard
                         </a>
-                        <a href="index.php?page=dashboard" class=" ml-1 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        <a href="index.php?page=add_blog" class=" ml-1 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
                             Add Blog
                         </a>
                         
