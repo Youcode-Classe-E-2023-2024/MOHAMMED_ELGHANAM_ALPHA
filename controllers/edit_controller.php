e71d4cab653f5027975ecede05a258d88c296581
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-01-08 02:18:35 +0100

    handel in page edite

--- controllers/edit_controller.php
+++ controllers/edit_controller.php
@@ -3,3 +3,7 @@ $objet_edit = new EDITE();
 $user_id = $_GET['user_id'];
 $result = $objet_edit->select($user_id);
 $row = mysqli_fetch_assoc($result);
+$name_update = $_POST['name'];
+$email_update = $_POST['email'];
+$password_update = $_POST['password'];
+$result1 = $objet_edit->edite($name_update,$email_update,$password_update);
--- models/edit_model.php
+++ models/edit_model.php
@@ -16,7 +16,15 @@ class EDITE{
 
     }
 
-    public function edit(){
+    public function edite($name_update,$email_update,$password_update){
+        global $db;
+        $sql = "UPDATE signup SET name = $name_update, email = $email_update, password = $password_update WHERE condition;";
+        $result = mysqli_query($db,$sql);
+        if (!$result) {
+            die("error in updating");
+        }else {
+            return $result;
+        }
 
     }
 
